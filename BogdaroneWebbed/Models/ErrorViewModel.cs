461504df170c98962e3c04e5ee853cc89cb3b17d
DariusEmanuel <dariusciuverca@gmail.com>
2021-12-02 12:02:55 +0200

    no message

---
+++ BogdaroneWebbed/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BogdaroneWebbed.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
